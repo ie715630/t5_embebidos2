d0caf0c94cadaac4fb0cc2793b0d5232cfb6fc9b
ie715630 <ie715630@iteso.mx>
2020-10-07 02:43:44 -0500

    Working version

--- alarm.c
+++ alarm.c
@@ -1,33 +1,3 @@
-/*
- * Copyright 2016-2020 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
  * @file    alarm.c
  * @brief   Application entry point.
@@ -52,6 +22,11 @@
 #define BIT_MINUTES (1 << 1)
 #define BIT_HOURS (1 << 2)
 
+#define INITIAL_SEC 55
+#define INITIAL_MIN 59
+#define INITIAL_HR 23
+
+
 typedef enum
 {
     seconds_type,
@@ -72,12 +47,12 @@ typedef struct
     uint8_t seconds;
 } alarm_t;
 
-void second_task(void *parameters);
-void minute_task(void *parameters);
-void hour_task(void *parameters);
+static void second_task(void *parameters);
+static void minute_task(void *parameters);
+static void hour_task(void *parameters);
 
-void alarm_task(void *parameters);
-void print_task(void *parameters);
+static void alarm_task(void *parameters);
+static void print_task(void *parameters);
 
 SemaphoreHandle_t minutes_semaphore;
 SemaphoreHandle_t hours_semaphore;
@@ -87,25 +62,22 @@ QueueHandle_t time_queue;
 EventGroupHandle_t alarm_event;
 SemaphoreHandle_t print_mutex;
 
+alarm_t alarm;
+
 int main(void)
 {
-    alarm_t alarm;
-
-    alarm.seconds = 0;
-    alarm.minutes = 0;
-    alarm.hours = 0;
+	alarm.seconds = 5;
+	alarm.minutes = 0;
+	alarm.hours = 0;
 
     /* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
     BOARD_InitDebugConsole();
-#endif
 
     xTaskCreate(second_task, "Seconds", configMINIMAL_STACK_SIZE, NULL,
-            configMAX_PRIORITIES - 4, NULL);
+    		configMAX_PRIORITIES - 4, NULL);
     xTaskCreate(minute_task, "Minutes", configMINIMAL_STACK_SIZE, NULL,
             configMAX_PRIORITIES - 4, NULL);
     xTaskCreate(hour_task, "Hours", configMINIMAL_STACK_SIZE, NULL,
@@ -125,19 +97,20 @@ int main(void)
     print_mutex = xSemaphoreCreateMutex();
     alarm_event = xEventGroupCreate();
 
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-           tight while() loop */
-        __asm volatile ("nop");
-    }
+    vTaskStartScheduler();
+
+   while(1){
+
+   }
+
     return 0 ;
 }
 
-void alarm_task(void *parameters)
+static void alarm_task(void *parameters)
 {
 
     const EventBits_t xBitsOfAlarm = BIT_SECONDS | BIT_MINUTES | BIT_HOURS;
+
 
     for(;;)
     {
@@ -146,20 +119,19 @@ void alarm_task(void *parameters)
                 pdTRUE, pdTRUE,
                 portMAX_DELAY);
         xSemaphoreTake(print_mutex, portMAX_DELAY);
-        PRINTF("Alarm\n");
+        PRINTF("\r\nAlarm");
         xSemaphoreGive(print_mutex);
     }
 }
 
-void print_task(void *parameters)
+static void print_task(void *parameters)
 {
-    static uint8_t local_secs = 0;
-    static uint8_t local_mins = 0;
-    static uint8_t local_hrs = 0;
+    static uint8_t local_secs = INITIAL_SEC;
+    static uint8_t local_mins = INITIAL_MIN;
+    static uint8_t local_hrs = INITIAL_HR;
 
     for(;;)
     {
-        xSemaphoreTake(print_mutex, portMAX_DELAY);
         time_msg_t received_msg_from_queue;
         xQueueReceive(time_queue,
                       &received_msg_from_queue,
@@ -177,15 +149,17 @@ void print_task(void *parameters)
                 local_hrs = received_msg_from_queue.value;
                 break;
         }
+        xSemaphoreTake(print_mutex, portMAX_DELAY);
+        PRINTF("\e[1;1H\e[2J");
         PRINTF("%02d:%02d:%02d", local_hrs, local_mins, local_secs);
         xSemaphoreGive(print_mutex);
     }
 }
 
 
-void second_task(void *parameters)
+static void second_task(void *parameters)
 {
-    static uint8_t counter_seconds = 0;
+    static uint8_t counter_seconds = INITIAL_SEC;
 
     for(;;)
     {
@@ -204,12 +178,18 @@ void second_task(void *parameters)
 
         TickType_t  xLastWakeTime = xTaskGetTickCount();
         TickType_t   xfactor = pdMS_TO_TICKS(1000);
+
+        if (counter_seconds == alarm.seconds)
+        {
+        	xEventGroupSetBits(alarm_event, BIT_SECONDS);
+        }
+
         vTaskDelayUntil(&xLastWakeTime, xfactor);
     }
 }
-void minute_task(void *parameters)
+static void minute_task(void *parameters)
 {
-    static uint8_t counter_minutes = 0;
+    static uint8_t counter_minutes = INITIAL_MIN;
 
     while(true)
     {
@@ -227,11 +207,16 @@ void minute_task(void *parameters)
         mins_msg.time_type = minutes_type;
         mins_msg.value = counter_minutes;
         xQueueSendToBack(time_queue, &mins_msg, 0);
+
+        if (counter_minutes == alarm.minutes)
+        {
+        	xEventGroupSetBits(alarm_event, BIT_MINUTES);
+        }
     }
 }
-void hour_task(void *parameters)
+static void hour_task(void *parameters)
 {
-    static uint8_t counter_hours = 0;
+    static uint8_t counter_hours = INITIAL_HR;
 
     while(true)
     {
@@ -247,5 +232,10 @@ void hour_task(void *parameters)
         hrs_msg.time_type = hours_type;
         hrs_msg.value = counter_hours;
         xQueueSendToBack(time_queue, &hrs_msg, 0);
+
+        if (counter_hours == alarm.hours)
+        {
+        	xEventGroupSetBits(alarm_event, BIT_HOURS);
+        }
     }
 }
